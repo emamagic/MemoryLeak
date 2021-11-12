6685d622320fb16278d5ea1f32f4faad88224829
emamagic <developermagic7@gmail.com>
2021-11-12 13:36:48 +0330

    nested class leak implemented

--- app/src/main/java/com/emamagic/memoryleak/nested_class_leak/NestedClassActivity.kt
+++ app/src/main/java/com/emamagic/memoryleak/nested_class_leak/NestedClassActivity.kt
@@ -2,11 +2,33 @@ package com.emamagic.memoryleak.nested_class_leak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 import com.emamagic.memoryleak.R
+import java.lang.ref.WeakReference
 
+private const val TAG = "NestedClassActivity"
 class NestedClassActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nested_class)
+        textView = findViewById(R.id.txt_nested_class_leak)
+
+        DownloadTask(textView).start()
+
     }
+
+    class DownloadTask(textView: TextView) : Thread() {
+        private val textView = WeakReference(textView)
+        override fun run() {
+            Handler(Looper.getMainLooper()).postDelayed({
+                textView.get()?.text = "Tadaaaa"
+            }, 30000)
+        }
+    }
+
 }
